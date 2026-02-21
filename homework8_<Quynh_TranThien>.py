a0fbb0c911f11626896613bc740354c85394ddf2
qtrant01 <qtrant01@uw.edu>
2026-02-21 22:19:43 +0000

    finished assignment added instructions

---
+++ homework8_<Quynh_TranThien>.py
@@ -0,0 +1,10 @@
+pizza_orders =["Margherita", "Pepperoni", "BBQ Chicken", "Veggie Supreme", "Hawaiian"]
+finished_pizzas = []
+
+for pizza in pizza_orders:
+    print("Your pizza is finished!")
+    finished_pizzas.append(pizza)
+
+for name in finished_pizzas:
+    print(f"The pizza {name} was made.")
+    
